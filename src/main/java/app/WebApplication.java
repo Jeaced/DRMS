8059c664996629f78ef0f7311c91c13411695fdb
loaferchassis <loaferchassis@gmail.com>
2018-04-09 19:24:23 +0300

    Fix pom file

--- src/main/java/app/WebApplication.java
+++ src/main/java/app/WebApplication.java
@@ -2,17 +2,10 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebApplication extends SpringBootServletInitializer {
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(WebApplication.class);
-    }
-
-    public static void main(String[] args) throws Exception {
+public class WebApplication {
+    public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
     }
 }
